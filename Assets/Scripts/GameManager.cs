3368dd4001ad4bf272e3b9e3e5ea0d215637a471
ahmadkhanraj01 <ahmadkhanraj01@gmail.com>
2025-08-25 23:24:01 +0500

    Flappy Bird  Assets and Scripts in unity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+
+    private int score = 0;
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+        playButton.SetActive(false);
+        Time.timeScale = 1f;
+        player.enabled = true;
+        gameOver.SetActive(false);
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for(int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+    public void GameOver()
+    {
+        score = 0;
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        Pause();
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+}
